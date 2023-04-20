2c14ce8349b3c2720221562ef811eb024c65886b
Ramo Molise <129608647+ramomolise@users.noreply.github.com>
2023-04-21 01:34:30 +0200

    Update docker_dockerCompose_armSetup.sh
    Docker Setup Script will now automatically setup VS code and Nginx Proxy Manager

--- bashScripts/docker_dockerCompose_armSetup.sh
+++ bashScripts/docker_dockerCompose_armSetup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Warning
+
+read -p "Make sure you modify VS Code Server Docker Compose File first before continuing. Press Enter to Continue or Ctrl-C to exit setup..." 
+
 # Installing docker
 
 curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
@@ -11,3 +15,12 @@ sudo usermod -aG docker $USER
 # Install docker-compose
 
 curl -L https://github.com/linuxserver/docker-docker-compose/releases/download/1.28.5-ls32/docker-compose-armhf | sudo tee /usr/local/bin/docker-compose >/dev/null
+
+cd ..
+cd dockerCompose/visualStudioCodeServer/
+docker compose up -d
+cd ..
+cd nginxProxyManager/
+docker compose up -d
+
+read -p "Nginx Proxy Manager and VS Code Server are Running...press Enter to continue..."
