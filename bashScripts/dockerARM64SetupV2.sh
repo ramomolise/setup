614b48f777612f4c583d704013890d67aa6cd8ad
Ramo Molise <129608647+ramomolise@users.noreply.github.com>
2023-04-21 10:57:49 +0200

    Create dockerARM64SetupV2.sh
    New Docker script for ARM64 Ubuntu 20.04

---
+++ bashScripts/dockerARM64SetupV2.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+# For Ubuntu 20.04
+
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+apt-cache policy docker-ce
+
+sudo apt-get -y install docker-ce
+
+sudo usermod -aG docker ${USER}
+
+sudo apt install python3-pip
+
+sudo pip3 install docker-compose
