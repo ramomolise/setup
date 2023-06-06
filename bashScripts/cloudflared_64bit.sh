93668457ccdb7bff91ec9daa574d7762745713c3
Ramo Molise <129608647+ramomolise@users.noreply.github.com>
2023-06-06 11:24:30 +0200

    added cloudflared for completely isolated environments

---
+++ bashScripts/cloudflared_64bit.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiZGRjODJmNmE5MGM4OThlOTQ2ZjI1MGI5YjliOGE4MTgiLCJ0IjoiN2QwZjljOWQtZDI2NS00N2VjLTlkMDYtZjk4ZjM1MDI2NzBjIiwicyI6Ik1XUTBZbVV6TldZdE1qUmpOaTAwWldaakxUZzJOVEF0T0dNd05qZ3hZakprWldRMSJ9
